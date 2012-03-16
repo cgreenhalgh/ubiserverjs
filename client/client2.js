978b359d2b937776bdc0a6ad2ea236fd98997a26
Chris Greenhalgh <cmg@cs.nott.ac.uk>
2012-03-16 22:46:54 +0000

    unknown peer connect implemented

--- client/client2.js
+++ client/client2.js
@@ -1,2 +1,189 @@
-// client2.js - client for ubiserver2
+// client2.js 
+// client for ubiserver2, flexible state sharing service, especially for mobile/multiuser ubicomp applications.
+//
+// requires socket.io 
+// See https://github.com/LearnBoost/socket.io-client
+// requires crypto-js: Crypto and MD5
+// See http://code.google.com/p/crypto-js
+//
 // Chris Greenhalgh, Copyright (c) University of Nottingham, 2012
+
+//=============================================================================
+// state etc.
+
+var version = 1;
+
+var device = new Object;
+var peer = new Object;
+
+
+var CONNECT_TIMEOUT = 5000;
+// states
+var STATE_NEW = 1;
+var STATE_PEER_REQ = 2;
+var STATE_PEERED = 3;
+
+//=============================================================================
+// util stuff
+
+//redefine for different logging
+var logmessage = function(direction,message,arguments) {
+	console.log('Message '+direction+' '+message+' '+JSON.stringify(arguments));
+}
+
+//get nonce (8 bytes), as byte array
+function getNonce() {
+	// Note: not secure!! replace if possible
+	return Crypto.util.randomBytes(8);
+}
+
+// get PIN as string
+var DIGITS = "0123456789";
+function getPin() {
+	var bytes = Crypto.util.randomBytes(4);
+	var pin = '';
+	for (var i in bytes) {
+		var byte = bytes[i];
+		pin += DIGITS.charAt((byte & 0xff) % 10);
+	}
+	return pin;
+}
+
+// perform MD5 digest, return as base64
+function getPinDigest(nonce, pin) {
+	var bytes = [];
+	for (var i in nonce)
+		bytes.push(nonce[i]);
+	var pinbytes = Crypto.charenc.UTF8.stringToBytes(pin);
+	for (i in pinbytes)
+		bytes.push(pinbytes[i]);
+	var digest = Crypto.MD5(bytes, {asBytes:true});
+	return Crypto.util.bytesToBase64(digest);
+}
+
+//=============================================================================
+
+function connect_socketio(device, peer) {
+	// Note: don't reconnect at the socket.io level - we'll do it at a higher level
+	// Note: if the initial handshake fails then we don't get any event back - we'd just have to 
+	// set a timeout for the lack of connecting.
+	var socket = io.connect('http://:49891', { transports: [ 'jsonp-polling' ], // 'websocket'
+		reconnect: false, 'connect timeout': 10000 });
+	peer.socket = socket;
+	peer.connected = false;
+
+	// timeout for initial handshake (inferred from first call to connect - could trap connecting but then 
+	// also deal with connect_failed, etc.)
+	peer.connectTimeout = setTimeout(function() {
+		logmessage('Event','connect timeout');
+	}, CONNECT_TIMEOUT);
+	
+	socket.on('connect', function() {
+		logmessage('Event','connect');
+		// cancel connect timeout
+		if (peer.connectTimeout!==undefined) {
+			clearTimeout(peer.connectTimeout);
+			delete peer.connectTimeout;
+		}
+		peer.connected = true;
+		peer.connstate = STATE_NEW;
+		if (!peer.known) {
+			// new/unknown peer
+			// send init_peer_req
+			// requires initiator nonce and pin
+			peer.nonce = getNonce();
+			peer.pin = getPin();
+			peer.pindigest = getPinDigest(peer.nonce, peer.pin);
+			console.log('nonce='+peer.nonce+', pin='+peer.pin+', pindigest='+peer.pindigest);
+			
+			// Note: we set 'unknown_peer' to prevent a resp_peer_known response
+			var m = {
+				type: 'init_peer_req',	
+				id: device.id,
+				pindigest: peer.pindigest,
+				name: device.name,
+				version: version,
+				reason: 'unknown_peer'
+			};
+			socket.json.send(m);
+			logmessage('Send', 'init_peer_req', m);
+			peer.connstate = STATE_PEER_REQ;
+		}
+		else {
+			// todo...
+			console.log('Connect to known peer unimplemented');
+			socket.disconnect();
+		}
+	});
+	socket.on('connecting', function(transport_type) {
+		logmessage('Event','connecting',transport_type);
+	});
+	socket.on('connect_failed', function() {
+		logmessage('Event','connect_failed','');
+	});
+	socket.on('close', function() {
+		logmessage('Event','close','');
+	});
+	// called when disconnect called or detected
+	socket.on('disconnect', function() {
+		logmessage('Event','disconnect','');
+		peer.connected = false;
+		delete peer.socket;
+	});
+	socket.on('reconnect', function(transport_type,reconnectionAttempts) {
+		logmessage('Event','reconnect',{transport_type:transport_type,reconnectionAttempts:reconnectionAttempts});
+	});
+	socket.on('reconnecting', function(reconnectionDelay,reconnectionAttempts) {
+		logmessage('Event','reconnecting',{reconnectionDelay:reconnectionDelay,reconnectionAttempts:reconnectionAttempts});
+	});
+	socket.on('reconnect_failed', function() {
+		logmessage('Event','reconnect_failed','');
+	});
+    socket.on('message', function (msg) {
+		logmessage('Recv','message',msg);
+		if (peer.connstate==STATE_PEER_REQ) {
+			if (msg.type=='resp_peer_nopin') {
+				// id, name, info? secret
+				peer.id = msg.id;
+				peer.name = msg.name;
+				peer.secret = msg.secret;
+				peer.connstate = STATE_PEERED;
+				console.log('Now peered with id='+peer.id+', name='+peer.name);
+			}
+			else if (msg.type=='resp_peer_known') {
+				// id, name, challenge1resp, challenge2
+				// but in this case we should have sent init_confirm !
+				
+			}
+			else {
+				console.log('state STATE_PEER_REQ got '+msg.type);
+				socket.disconnect();
+				return;
+			}
+
+		}
+    });
+    
+}
+
+
+// called to initiate (high-level) connectivity to server
+function connect(id, name, group) {
+	// old connection?
+	disconnect();
+	peer.known = false;
+	// ...
+	logmessage('Action','connect',{id:id,name:name,group:group});
+	device.id = id;
+	device.name = name;
+	device.group = group;
+	
+	connect_socketio(device, peer);
+}
+
+function disconnect() {
+	if (peer.socket!==undefined) {
+		peer.socket.disconnect();
+		logmessage('Sent', 'disconnect');
+	}
+}
--- server/ubiserver2.js
+++ server/ubiserver2.js
@@ -1,4 +1,8 @@
 // ubiserver2.js
+// Flexible state sharing service, especially for mobile/multiuser ubicomp applications.
+//
+// requires socket.io,  connect and cryptojs
+//
 // see https://github.com/cgreenhalgh/ubihelper/blob/master/docs/Ubiserver2DesignNotes.html
 // Chris Greenhalgh, Copyright (c) University of Nottingham, 2012
 
@@ -8,7 +12,10 @@
 // modules: connect, http, socket.io
 var connect = require('connect'),
 	http = require('http'),
-    io = require('socket.io');
+    io = require('socket.io'),
+    os = require('os');
+
+var Crypto = (require ('cryptojs')).Crypto;
 
 // set up the connect bit, e.g. static pages
 var app = connect()
@@ -23,9 +30,108 @@ io = io.listen(httpapp);
 httpapp.listen(49891);
 
 //======================================================================
+// server state
+
+// peers, indexed by peerid, value is peer 
+var peers = new Object;
+
+var device = new Object;
+device.id = 'ubiserver2:'+os.hostname()+':'+(new Date().getTime());
+device.name = 'Ubserver2 on '+os.hostname();
+
+var nextConn = 1;
+
+var STATE_NEW = 1;
+var STATE_PEERED = 2;
+var STATE_KNOWN_CHALLENGED = 3;
+
+var version = 1;
+
+//======================================================================
 
 //new socket.io client
 io.sockets.on('connection', function(socket) {
-	// ...
+	// state
+	var conn = new Object;
+	var serverid = nextConn++;
+	conn.serverid = serverid;
+	conn.state = STATE_NEW;
+	
+	// register listeners 
+	console.log('new socket.io connection '+serverid);
+	
+	socket.on('message', function (msg) { 
+		console.log('message from '+serverid+': '+JSON.stringify(msg));
+		if (conn.state==STATE_NEW) {
+			if (msg.type!='init_peer_req') {
+				console.log('state NEW required init_peer_req; got '+msg.type);
+				socket.disconnect();
+				return;
+			}
+			// id, pindigest, name, version, reason?
+			if (msg.version!=version) {
+				socket.json.send({type:'reject',reason:'unsupported_version',message:'Version not supported (server version '+version+', client version '+msg.version+')'});
+				socket.disconnect();
+				return;
+			}
+			var peer = peers[msg.id];
+			if (peer===undefined) {
+				// new peer - return resp_peer_nopin
+				peer = new Object;
+				peer.socket = socket;
+				peer.id = msg.id;
+				peer.name = msg.name;
+				// needs secret
+				peer.secret = Crypto.util.randomBytes(8);
+				peer.state = STATE_PEERED;
+				peer.serverid = serverid;
+				// add to peers
+				peers[peer.id] = peer;
+				// TODO: timeout/soft state?
+				// also: port, info
+				var resp = { type: 'resp_peer_nopin', id: device.id, name: device.name,
+						secret: Crypto.util.bytesToBase64(peer.secret) };
+				console.log('New peer (no pin) id='+peer.id+', name='+peer.name);
+				socket.json.send(resp);
+				return;
+			} 
+			else {
+				// known peer - could return resp_peer_known
+				if (msg.reason!==undefined) {
+					// in this case resp_peer_known has/will fail. So this is effectively a new 
+					// peer with a known peer id. That is really bad, so we return an error.
+					console.log('Reject init_peer_req with reason='+msg.reason+' for known peer '+msg.id);
+					socket.json.send({type:'reject',reason:'known_peer',message:'This peer ID is already known but client refuses or fails challenge'});
+					socket.disconnect();
+					return;
+				}
+				console.log('Reject init_peer_req for known peer '+msg.id+' - should not happen');
+				socket.json.send({type:'reject',reason:'known_peer',message:'This peer ID is already known but client has not used init_confirm'});
+				socket.disconnect();
+				return;
+/*				conn.id = msg.id;
+				conn.state = STATE_KNOWN_CHALLENGED;
+				// needs challenge1resp = base64-encoded MD5 digest of pindigest and secret
+				var bytes = [];
+				var pindigest = Crypto.util.base64ToBytes(msg.pindigest);
+				var challenge1resp = bytes.concat(pindigest, peer.secret);			
+				// and challenge2 = base64-encoded challenge
+				conn.challenge2 = Crypto.util.randomBytes(8);
+				// also: port, info
+				var resp = { type: 'resp_peer_known', id: device.id, name: device.name,
+						challenge1resp: Crypto.util.bytesToBase64(challenge1resp),
+						challenge2: Crypto.util.bytesToBase64(conn.challenge2) };
+				console.log('Issued challenge to known peer id='+peer.id+', name='+peer.name);
+				socket.json.send(resp);
+*/				return;				
+			}
+		}
+		// ...
+	});
+
+	socket.on('disconnect', function () { 
+		console.log('disconnect from '+serverid);
+		// ...
+	});
 });
 
