2396de0c8e40bddc08245900ec1eca8eb29c9787
Chris Greenhalgh <cmg@cs.nott.ac.uk>
2012-03-21 20:55:19 +0000

    socket.io bootstrap example

---
+++ client/client.js
@@ -0,0 +1,10 @@
+// client javascript
+// requires socket.io and jQuery
+
+// standard socket.io example
+
+var socket = io.connect('http://localhost');
+socket.on('news', function (data) {
+  console.log(data);
+  socket.emit('my other event', { my: 'data' });
+});
---
+++ server/server.js
@@ -0,0 +1,9 @@
+//standard socket.io sample
+var io = require('socket.io').listen(49891);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
