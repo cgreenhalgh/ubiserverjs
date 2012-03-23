54a79488c49c171d6e3f6b867cf24aad664aa2a0
Chris Greenhalgh <cmg@cs.nott.ac.uk>
2012-03-23 14:04:34 +0000

    initial working server and ajax client

--- client/client.js
+++ client/client.js
@@ -3,8 +3,85 @@
 
 // standard socket.io example
 
-var socket = io.connect('http://:49891');
-socket.on('news', function (data) {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
-});
+var socket;
+
+var version = 1;
+
+// redefine for different logging
+var logmessage = function(direction,message,arguments) {
+	console.log('Message '+direction+' '+message+' '+JSON.stringify(arguments));
+}
+
+// create connection...!
+function connect(clientname,group,log,connectcallback,valuecallback) {
+	socket = io.connect('http://:49891');
+	socket.on('connect', function() {
+		// hello
+		var newclient = { version: version, name: clientname, g: group, log: ((log!==undefined && log) ? 1 : 0) };
+		socket.emit('ubiserver.newclient',  newclient);
+		logmessage('Sent','ubiserver.newclient', newclient);
+	});
+	socket.on('ubiserver.welcome', function(welcome) {
+		logmessage('Recv','ubiserver.welcome', welcome);
+		connectcallback && connectcallback(true,welcome);
+	});
+	socket.on('ubiserver.reject', function(reject) {
+		logmessage('Recv', 'ubiserver.reject', reject);
+		socket = undefined;
+		connectcallback && connectcallback(false,reject);
+	})
+	socket.on('disconnect', function() {
+		logmessage('Recv', 'disconnect', null);
+		socket = undefined;
+		connectcallback && connectcallback(false,null);
+	});
+	socket.on('v', function(value) {
+		logmessage('Recv', 'v', value);
+		valuecallback && valuecallback(value);
+	});
+}
+
+function set(n, v, p, c, e) {
+	if (socket!=undefined) {
+		var s = { n: n };
+		if (v!==undefined)
+			s.v = v;
+		if (p!==undefined)
+			s.p = p;
+		if (c!==undefined)
+			s.c = c;
+		if (e!==undefined)
+			s.e = e;
+		socket.emit('s', s);
+		logmessage('Send', 's', s);
+	}
+}
+
+function join(n, p) {
+	if (socket!==undefined) {
+		var join = { n : n };
+		if (p!==undefined)
+			join.p = 1;
+		socket.emit('j', join);
+		logmessage('Send', 'j', join);
+	}
+}
+
+function leave(n) {
+	if (socket!==undefined) {
+		var leave = { n : n };
+		socket.emit('l', leave);
+		logmessage('Send', 'l', leave);
+	}
+}
+
+function disconnect() {
+	if (socket!==undefined) {
+		socket.disconnect();
+		logmessage('Sent', 'disconnect');
+	}
+}
+//socket.on('news', function (data) {
+//  console.log(data);
+//  socket.emit('my other event', { my: 'data' });
+//});
--- server/server.js
+++ server/server.js
@@ -45,6 +45,35 @@ function rejectclient(socket, clientid, reason) {
 	socket.disconnect();
 }
 
+// remove a client - in a group
+function removeclient(socket, clientid, g) {
+	var gcs = groupclients[g];
+	if (gcs!==undefined) 
+		delete gcs[clientid];
+	// TODO GC groups?
+}
+
+
+//reject a client - in a group
+function failclient(socket, clientid, g, reason) {
+	console.log('failclient '+clientid+': '+reason);
+	socket.emit('ubiserver.reject', { version: version, name: servername, reason: 'version='+version});
+	socket.disconnect();
+	removeclient(socket, clientid, g);
+}
+
+function matches(sub, name) {
+	return sub.test(name);
+}
+function checkvalue(socket, sub, n, val, c) {
+	if (matches(sub, n)) {
+		var value = { n : n, t : new Date().getTime(), v : val };
+		if (c!==undefined)
+			value.c = c;
+		socket.emit('v', value);
+	}
+}
+
 // socket.io client id
 var nextclientid = 1;
 
@@ -77,6 +106,15 @@ io.sockets.on('connection', function(socket) {
 		var cstate = new Object;
 		socket.set('state', cstate);
 		
+		// client subscriptions
+		var subs = [];
+		socket.set('subs', subs);
+		
+		// test
+		socket.get('subs', function(err, ss) {
+			console.log('subs='+JSON.stringify(ss));
+		});
+		
 		// add to group set of clients
 		var gcs = groupclients[g];
 		if (gcs===undefined) {
@@ -84,26 +122,166 @@ io.sockets.on('connection', function(socket) {
 			groupclients[g] = gcs;
 		}
 		gcs[clientid] = socket;
-		
-		console('added client '+clientid+': g='+g+', name='+name+', log='+log);
+
+		// group state
+		var gstate = groupstates[g];
+		if (gstate===undefined) {
+			gstate = new Object;
+			groupstates[g] = gstate;
+		}
+
+		console.log('added client '+clientid+': g='+g+', name='+name+', log='+log);
 		
 		// ok
 		socket.emit('ubiserver.welcome', { version: version, name: servername, c: clientid });
 
 		// listeners...
 		socket.on('s', function(set) {
+			if (set.n===undefined) {
+				failclient(socket, clientid, g, 's message had no "n" (name) element');
+				return;
+			}
+			var c = set.c;
+			if (c!==undefined) {
+				if (c.length==0)
+					c = clientid;
+				else
+					c = clientid+'/'+c;
+			}
+			if (set.p) {
+				// save
+				var state;
+				if (c===undefined) {
+					// g scope
+					state = gstate;
+					console.log('set group '+g+' '+set.n+'='+set.v);
+				} else {
+					// client/sub scrope
+					state = cstate[c];
+					if (state===undefined) {
+						state = new Object;
+						cstate[c] = state;
+					}					
+					console.log('set group '+g+' client '+c+' '+set.n+'='+set.v);
+				}
+				if (set.v===undefined)
+					delete state[set.n];
+				else
+					state[set.n] = set.v;
+			}// done set
 			
+			// value
+			var value = { n : set.n, t : new Date().getTime() };
+			if (set.v!==undefined)
+				value.v = set.v;
+			if (c!==undefined)
+				value.c = c;
+			
+			// match/send
+			for (var cid in gcs) {
+				// clients in group
+				var c = gcs[cid];
+				if (c===socket && !(set.e)) 
+					continue;
+				
+				c.get('subs', function(err, subs) {
+					if (subs) {
+						for (var i=0; i<subs.length; i++) {
+							var sub = subs[i];
+							if (matches(sub, set.n))
+								c.emit('v', value);
+						}
+					}
+					else 
+						console.log('Error getting subs for '+cid+': '+err);
+				});
+			}
 		});
 		socket.on('j', function(join) {
+			if (join.n===undefined) {
+				failclient(socket, clientid, g, 'j message had no "n" (name) element');
+				return;
+			}
 			
+			console.log('Join for '+clientid+' ('+g+') n='+join.n);
+			var sub = new RegExp('^'+join.n+'$')
+			subs.splice(subs.length, 0, sub);
+			
+			if (join.p) {
+				// group state
+				for (var n in gstate) {
+					checkvalue(socket, sub, n, gstate[n]);
+				}
+				// group client state
+				for (var cid in gcs) {
+					var client = gcs[cid];
+					if (client===socket)
+						continue;// not self
+					client.get('state', function (err, cstate) {						
+						for (var c in cstate) {
+							var st = cstate[c];
+							for (var n in st) {
+								checkvalue(socket, sub, n, st[n], c);
+							}
+						}
+					});
+				}
+			}
 		});
 		socket.on('l', function(leave) {
-			
+			if (leave.n===undefined) {
+				failclient(socket, clientid, g, 'l message had no "n" (name) element');
+				return;
+			}
+			var done = false;
+			var pat = '^'+leave.n+'$';
+			for (var ix=0; ix<subs.length; ix++) {
+				var sub = subs[ix];
+				if (pat==sub.source) {
+					console.log('Leave for '+clientid+' ('+g+') n='+leave.n);
+					subs.splice(ix, 1);
+					done = true;
+					break;
+				}
+			}
+			if (!done) {
+				console.log('Leave unknown for '+clientid+' n='+leave.n);
+				return;
+			}
+		});
+		// client disconnect handling
+		socket.on('disconnect', function() {
+			console.log('disconnect from '+clientid);
+			removeclient(socket, clientid, g);
+			// clear persistent client values to subscriptions
+			for (var cid in cstate) {
+				var st = cstate[cid];
+				for (var n in st) {
+					// value
+					var value = { n : n, t : new Date().getTime(), c: cid };
+					// match/send
+					for (var cid in gcs) {
+						// clients in group
+						var c = gcs[cid];
+						if (c===socket) 
+							continue;
+						
+						c.get('subs', function(err, subs) {
+							if (subs) {
+								for (var i=0; i<subs.length; i++) {
+									var sub = subs[i];
+									if (matches(sub, n))
+										c.emit('v', value);
+								}
+							}
+							else 
+								console.log('Error getting subs for '+cid+': '+err);
+						});
+					}
+
+				}
+			}
 		});
 	});
 	
-	// client disconnect handling
-	socket.on('disconnect', function() {
-		
-	});
 });
